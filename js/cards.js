832c2683fe72c12eb9fbce92ec22f73f5e5c04c0
Abu-alwafa <abu.alwafa892@gmail.com>
2021-06-26 05:36:52 +0300

    upload project files

---
+++ js/cards.js
@@ -0,0 +1,76 @@
+
+const dObj = [
+    {
+        imgSrc: 'imgs/1.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/2.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/3.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/4.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/5.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/6.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/4.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/5.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/6.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    },
+    {
+        imgSrc: 'imgs/2.jpg',
+        title: 'Dress',
+        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sed?'
+    }
+   
+];
+
+const cards = document.querySelector('.products-page .cards');
+
+for (let i of dObj) {
+    let card = `
+            <div class="card">
+                <div class="img-container">
+                    <img src="${dObj[dObj.indexOf(i)].imgSrc}" alt="">
+                </div>
+                <div class="details">
+                    <div class="text">
+                        <h3>${dObj[dObj.indexOf(i)].title}</h3>
+                        <p>${dObj[dObj.indexOf(i)].describe}</p>
+                    </div>
+                    <div class="request">
+                        <input type="button" value="Request">
+                    </div>
+                </div>
+            </div>
+`;
+    cards.innerHTML += card;
+}
---
+++ js/master.js
@@ -0,0 +1,189 @@
+// resize website pages corresponding devise height
+const resizeHeight = () => {
+    document.querySelector('.landing-page').style.height = window.innerHeight + 'px';
+    document.querySelector('.services-page').style.height = window.innerHeight + 'px';
+    document.querySelector('.contact-page').style.height = window.innerHeight + 'px';
+}
+resizeHeight();
+window.onresize = resizeHeight;
+// jquery smooth scroll 
+$('.links li a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    };
+});
+// Main Function here
+const main = () => {
+    // Selecting Elements function
+    const $ = (elem) => { return document.querySelector(elem); };
+    const $$ = (elements) => { return document.querySelectorAll(elements) };
+    // Function for click Events
+    const clickEvent = (element, func) => {
+        return element.addEventListener("click", func);
+    }
+    // Function for add class to element
+    const addClass = (element, clas) => {
+        return element.classList.add(clas);
+    }
+    // Function for remove class from element
+    const removeClass = (element, clas) => {
+        return element.classList.remove(clas);
+    }
+    // Function for change activility to header links whin user scroll
+    const changeActiveLink = (elements, linkName) => {
+        return elements.forEach(e => {
+            removeClass(e, 'active');
+            if (e.dataset.name === linkName) addClass(e, "active");
+        })
+    }
+    //function to change Activelity to Elements
+    const changeActiveElement = (elements, element) => {
+        return elements.forEach(e => {
+            removeClass(e, 'active');
+            if (e == element) addClass(e, "active");
+        })
+    }
+    //Get array of amgs
+    const imgsArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+    const serviceImgs = ["6.jpg", "7.png"];
+    // let names for assinment to intervals
+    let normalInterval, randomInterval;
+    // 2 function for changing background images
+    function ChangeBkRan(page, time = 5000) {
+        let i = 0;
+        this.page = page;
+        this.time = time;
+        randomInterval = setInterval(() => {
+            let randomNums = Math.floor(Math.random() * imgsArray.length);
+            //Change background img url
+            this.page.style.backgroundImage = 'url("imgs/' + imgsArray[randomNums] + '")';
+        }, this.time);
+        console.log("random");
+    }
+    function ChangeBkNor(page, time = 5000, arr = imgsArray) {
+        let i = 0;
+        this.page = page;
+        this.time = time;
+        // Interval for switch background at custom time
+        normalInterval = setInterval(() => {
+            //Change background img url
+            this.page.style.backgroundImage = 'url("imgs/' + arr[i] + '")';
+            i = (i + 1) % arr.length;
+        }, this.time)
+        console.log("normal");
+    }
+    // add click to menu icon for open and close nav bar
+    clickEvent($('.menu-icon'), () => {
+        $('.links').classList.toggle('show');
+        $('.menu-icon').classList.toggle('toggle');
+        $$('.links li').forEach((link, index) => {
+            if (link.style.animation) link.style.animation = '';
+            else link.style.animation = `slideIn 1s ease`
+        })
+
+    })
+    // add click to toggle cog for opening setting box
+    clickEvent($('.toggle-settings'), () => {
+        $('.settings-box').classList.toggle('open');
+        $(".toggle-settings > .fa-cog").classList.toggle('fa-spin');
+        $$('.settings-container > div').forEach((div, index) => {
+            if (div.style.animation) div.style.animation = '';
+            else div.style.animation = `slideInFromLeft 1s ease`
+        })
+    })
+    // Add click to landing page for closing setting box
+    clickEvent($('.overlay'), () => {
+        removeClass($('.settings-box'), 'open')
+        removeClass($('.toggle-settings > .fa-cog'), 'fa-spin');
+    })
+    // Add click to each color circle for changing root color and setting value in local storage
+    $$('.color-list li').forEach(item => clickEvent(item, (e) => {
+        $(':root').style.setProperty("--main--color", e.target.dataset.color);
+        changeActiveElement($$('.color-list li'), e.target)
+        localStorage.setItem('colorSetting', e.target.dataset.color);
+    }))
+    // Checking localStoragr if it is storing a color value
+    let colorOption = localStorage.getItem('colorSetting');
+    if (colorOption !== null) {
+        $(':root').style.setProperty("--main--color", colorOption);
+        // Check color list items and set active class to it
+        $$(".color-list li").forEach(e => {
+            removeClass(e, 'active');
+            if (e.dataset.color === colorOption) addClass(e, 'active');
+        });
+    }
+    // Add click to each yes and no buttons for changing activelity 
+    $$('.yes-no li').forEach(button => button.addEventListener('click', (e) => {
+        changeActiveElement($$('.yes-no li'), e.target)
+        if (e.target.dataset.random === "yes") {
+            clearInterval(normalInterval);
+            clearInterval(randomInterval);
+            ChangeBkRan($('.landing-page'));
+        }
+        else {
+            clearInterval(randomInterval);
+            clearInterval(normalInterval);
+            ChangeBkNor($('.landing-page'));
+        }
+        $$('.time-groub li').forEach(e => { if (e.dataset.time === '5000') changeActiveElement($$('.time-groub li'), e); });
+        localStorage.setItem("isRandomlyImgs", e.target.dataset.random);
+    }))
+
+    // add click event to time groub buttons for changing swithTime value
+    $$('.time-groub li').forEach(button => clickEvent(button, (e) => {
+        changeActiveElement($$('.time-groub li'), e.target);
+        changeSwitchTime(e.target.dataset.time);
+    }))
+    // add scroll event to window 
+    window.onscroll = function (e) {
+        // when user scroll close setting box and unRotate cog icon
+        removeClass($('.settings-box'), 'open');
+        removeClass($(".toggle-settings > .fa-cog"), 'fa-spin');
+        // checking page offset for hide and appear into text
+        let scrollAmount = this.pageYOffset;
+        if (scrollAmount > 100) {
+            addClass($('.introduction-text'), 'hidden')
+        }
+        else {
+            removeClass($('.introduction-text'), 'hidden');
+        }
+
+        // check scroll amount for changing active link(page)
+        if (scrollAmount < ($('.services-page').offsetTop)) {
+            changeActiveLink($$('.links li a'), 'about');
+        }
+        else if (scrollAmount >= $('.services-page').offsetTop && scrollAmount < $('.products-page').offsetTop) {
+            changeActiveLink($$('.links li a'), 'services');
+        }
+        else if (scrollAmount >= $('.products-page').offsetTop && scrollAmount < $('.contact-page').offsetTop) {
+            changeActiveLink($$('.links li a'), 'products');
+        }
+        else changeActiveLink($$('.links li a'), 'contact');
+    }
+    let randomImgsOption = localStorage.getItem('isRandomlyImgs');
+    const changeSwitchTime = (time = 5000) => {
+        // Checking localStorage if it is storing a IsRandomlyImgs value
+        if (randomImgsOption !== null) {
+            clearInterval(normalInterval);
+            clearInterval(randomInterval);
+            if (randomImgsOption === "yes") {
+                ChangeBkRan($('.landing-page'), Number(time));
+            } else {
+                ChangeBkNor($('.landing-page'), Number(time));
+            }
+        } else ChangeBkNor($('.landing-page'));
+    }
+
+
+    changeSwitchTime();
+
+}
+main();
+
+
+
+
